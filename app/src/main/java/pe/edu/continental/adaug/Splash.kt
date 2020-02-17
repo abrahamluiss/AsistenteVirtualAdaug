0571bef5248a6984d5385979d40d44162d157a0e
abrahamluiss <abraham105@hotmail.com>
2020-02-17 18:12:10 -0500

    Programacion de asistent.kt

---
+++ app/src/main/java/pe/edu/continental/adaug/Splash.kt
@@ -0,0 +1,12 @@
+package pe.edu.continental.adaug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
