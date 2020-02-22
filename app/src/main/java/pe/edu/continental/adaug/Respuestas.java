afcde437b30e6c7b8d468ff98f39e48026009556
abrahamluiss <abraham105@hotmail.com>
2020-02-22 01:23:25 -0500

    asistente en java agregado sin api

---
+++ app/src/main/java/pe/edu/continental/adaug/Respuestas.java
@@ -0,0 +1,19 @@
+package pe.edu.continental.adaug;
+
+public class Respuestas {
+    private String cuestion;
+    private String respuestas;
+
+    public Respuestas(String cuestion, String respuestas) {
+        this.cuestion = cuestion;
+        this.respuestas = respuestas;
+    }
+
+    public String getCuestion() {
+        return cuestion;
+    }
+
+    public String getRespuestas() {
+        return respuestas;
+    }
+}
